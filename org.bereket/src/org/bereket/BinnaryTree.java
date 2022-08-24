d6a61179efecb1f5de55620ec8a2c8f05cd548c8
bwoldemi <bereketka@gmail.com>
2022-08-24 09:53:14 +0300

    creating project

---
+++ org.bereket/src/org/bereket/BinnaryTree.java
@@ -0,0 +1,11 @@
+package org.bereket;
+
+public class BinnaryTree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Binnary Tree");
+
+	}
+
+}
